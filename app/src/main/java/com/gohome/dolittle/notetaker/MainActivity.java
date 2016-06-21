df6743c96f9164dce53af07305963eac83fa695a
donaldsec42 <donaldsec42@gmail.com>
2016-06-21 11:52:19 -0400

    Save Button functions

--- app/src/main/java/com/gohome/dolittle/notetaker/MainActivity.java
+++ app/src/main/java/com/gohome/dolittle/notetaker/MainActivity.java
@@ -3,6 +3,9 @@ package com.gohome.dolittle.notetaker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = (EditText)findViewById(R.id.titleEditText);
+                EditText noteEditText = (EditText)findViewById(R.id.noteEditText);
+
+                titleEditText.setEnabled(false);
+                noteEditText.setEnabled(false);
+            }
+        });
     }
 
     @Override
